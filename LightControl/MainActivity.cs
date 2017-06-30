95a3c53bc71eb84591070f8e9763c367a2be556c
Marius Vasshus <marvas@lyse.net>
2017-06-30 23:09:01 +0200

    Started working on navigation drawer. Currently it slides in and out.

--- LightControl/MainActivity.cs
+++ LightControl/MainActivity.cs
@@ -1,18 +1,41 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
 
 namespace LightControl
 {
     [Activity(Label = "LightControl", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        DrawerLayout drawerLayout;
+        List<string> leftItems = new List<string>();
+        ArrayAdapter leftAdapter;
+        ListView leftDrawer;
+        ActionBarDrawerToggle drawerToggle;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            drawerLayout = FindViewById<DrawerLayout> (Resource.Id.leftDrawer);
+            leftDrawer = FindViewById<ListView> (Resource.Id.leftListView);
+
+            leftItems.Add("First left item");
+            leftItems.Add("Second left item");
+
+            //drawerToggle = new ActionBarDrawerToggle (this, drawerLayout, Resource.Drawable.ic_view_headline_black_24dp, Resource.String.ns_menu_close);
+
+            leftAdapter = new ArrayAdapter (this, Android.Resource.Layout.SimpleListItem1, leftItems);
+            leftDrawer.Adapter = leftAdapter;
+
+            //drawerLayout.SetDrawerListener(drawerToggle);
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
+            ActionBar.SetHomeButtonEnabled(true);
         }
     }
 }
