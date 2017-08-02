f3cf757a184ce7bc3383cd7cd15c9b8c8c42e1be
Marius Vasshus <marvas@lyse.net>
2017-08-02 19:00:19 +0200

    Worked on maps

--- LightControl/PoiFragment.cs
+++ LightControl/PoiFragment.cs
@@ -10,11 +10,15 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
 
 namespace LightControl
 {
-    public class PoiFragment : Fragment
+    public class PoiFragment : Fragment, IOnMapReadyCallback
     {
+        private GoogleMap gMap;
+        MapView mapView;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,14 +30,54 @@ namespace LightControl
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            return inflater.Inflate(Resource.Layout.Poi, container, false);
+            View inflatedView = inflater.Inflate(Resource.Layout.Poi, container, false);
+
+            mapView = (MapView)inflatedView.FindViewById(Resource.Id.map);
+            mapView.OnCreate(savedInstanceState);
+            mapView.OnResume();
+
+            try
+            {
+                MapsInitializer.Initialize(Activity.ApplicationContext);
+            }
+            catch (Exception e)
+            {
+                //IMPLEMENT
+            }
+            
+            mapView.GetMapAsync(this);
+
+            return inflatedView;
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            gMap = googleMap;
         }
 
         public override void OnResume()
         {
             this.Activity.ActionBar.Title = "Points of interest";
             base.OnResume();
+            mapView.OnResume();
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            mapView.OnPause();
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            mapView.OnDestroy();
+        }
+
+        public override void OnLowMemory()
+        {
+            base.OnLowMemory();
+            mapView.OnLowMemory();
         }
     }
 }
